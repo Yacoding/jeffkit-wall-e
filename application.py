e11c9c74b0b87232957f6f7c84c79267a51ccd2b
Sven <474416133@qq.com>
2011-03-25 14:28:08 +0800

    1.import module at line 5 2.add 'rh.hl = xmlHandler()' in line 11 3.change the lamda function

--- application.py
+++ application.py
@@ -2,13 +2,14 @@
 
 import urllib,urllib2
 from monitor import Monitor
-
+from resultHandler import xmlHandler
 if __name__ == '__main__':
     # 演示结果处理器可以配置
     class Handler:
         pass
     rh = Handler()
-    setattr(rh,'handle',lambda x:log.debug('handle result in your way!'))
+    rh.hl = xmlHandler()
+    setattr(rh,'handle',lambda x:rh.hl.handle(x))
     #raise Exception,"just test"
     Monitor(result_handler=rh).run()
 
