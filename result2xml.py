4317adaecf86d6b5ed2ed0274702e0469e29e868
Sven <474416133@qq.com>
2011-03-30 09:41:35 +0800

    1.del function xml_soappsample_detail 2.del function xml_httpsample_detail

--- result2xml.py
+++ result2xml.py
@@ -1,6 +1,8 @@
 import traceback
+import sys
 from xml.dom import minidom
-#xml_doc = minidom.Document()
+from logger import log,log_exce
+
 def create_xml_doc():
     return  minidom.Document()
 
@@ -45,90 +47,6 @@ def xml_assert(result,headnode,xml_doc):
     headnode.appendChild(child)
     return child
 
-def xml_soapsample_detail(rs,headnode,xml_doc):
-    if getattr(rs,'soapRespone',None):
-        from SOAPpy import Types
-	child_rs_soap = xml_doc.createElement('soap_Result')
-	if type(rs.soapRespone) in [tuple,list]:
-	    child_rs_soap_attr = xml_doc.createElement("list")
-	    for item in rs.soapRespone:
-		if item.__class__ is Types.structType:
-	            child_rs_soap_attr.setAttribute('name',item._name)
-		    child_rs_soap_attr_list = xml_doc.createElement("attribute")
-		    for i in item.__dict__.items():
-			if '_' not in i[0]:
-			    child_rs_soap_attr_list.setAttribute(i[0],i[1])
-		    child_rs_soap_attr.appendChild(child_rs_soap_attr_list)
-		else:
-                    child_rs_soap_text = xml_doc.createTextNode(item)
-                    child_rs_soap.appendChild(child_rs_soap_text)
-	    child_rs_soap.appendChild(child_rs_soap_attr)
-
-	elif rs.soapRespone.__class__ is Types.structType:
-	    child_rs_soap_attr = xml_doc.createElement("attribute")
-	    child_rs_soap_attr.setAttribute('name',rs.soapRespone._name)
-	    child_rs_soap_attr.setAttribute('type',str(rs.soapRespone.__class__))
-	    print rs.soapRespone.__dict__.items()
-	    for item in rs.soapRespone.__dict__.items():
-	        if '_' not in item[0]:
-		    print item[0].ljust(20),str(item[1])
-                    child_rs_soap_attr.setAttribute(item[0],str(item[1]))
-	    child_rs_soap.appendChild(child_rs_soap_attr)
-	else:
-	    child_rs_soap_text = xml_doc.createTextNode(str(rs.soapRespone))
-            child_rs_soap.appendChild(child_rs_soap_text)
-	headnode.appendChild(child_rs_soap)
-
-def xml_httpsample_detail(rs,headnode,xml_doc):
-    if rs.__dict__.has_key('httpHeader'):
-        child_rs_httpheader = xml_doc.createElement('httpHeader')
-        for item in rs.httpHeader:
-	    child_rs_httpheader_attr =  xml_doc.createElement("attribute")
-	    child_rs_httpheader_attr.setAttribute('name',item[0])
-            child_rs_httpheader_attr.setAttribute('value',item[1])
-            child_rs_httpheader.appendChild(child_rs_httpheader_attr)
-        headnode.appendChild(child_rs_httpheader)
-    if rs.__dict__.has_key('responseHeaders'):
-        child_rs_resheader = xml_doc.createElement('responseHeader')
-        for rsitem in rs.responseHeaders.__dict__.items():
-	    child_rs_resheader_attr =  xml_doc.createElement("attribute")
-            child_rs_resheader_attr.setAttribute('name',str(rsitem[0]))
-	    if type(rsitem[1]) is list:
-		for i in rsitem[1]:
-	            if ':' in i:
-		        child_rs_resheader_attr_list =  xml_doc.createElement("key")
-                        key = i[:i.index(':')]
-		        value = i[i.index(':')+1:]
-			child_rs_resheader_attr_list.setAttribute(key,value)
-                        child_rs_resheader_attr.appendChild(child_rs_resheader_attr_list)
-	    elif type(rsitem[1]) is dict:
-		for i in rsitem[1].items():
-		    child_rs_resheader_attr_list =  xml_doc.createElement("key")				
-		    child_rs_resheader_attr_list.setAttribute(i[0],i[1])
-                    child_rs_resheader_attr.appendChild(child_rs_resheader_attr_list)
-            else:
-		child_rs_resheader_attr.setAttribute('value',str(rsitem[1]))
-            child_rs_resheader.appendChild(child_rs_resheader_attr)
-        headnode.appendChild(child_rs_resheader)
-    if rs.__dict__.has_key('responseText'):
-	def getCharset():
-	    content_type = rs.responseHeaders.dict['content-type']
-	    charsets = content_type.split(';')
-	    for item in charsets:
-		if 'charset=' in item:
-	            if 'gb' in item.lower():
-			return 'gbk'
-		    else:
-			return item[item.index('=')+1:]
-	charset = getCharset()
-        child_rs_resText = xml_doc.createElement('responseText')
-        data = rs.responseText.decode(charset)
-	if ']]>' in data:
-	    data = data.replace(']]>',']] >')
-        child_rs_resText_text = xml_doc.createCDATASection(data)
-        child_rs_resText.appendChild(child_rs_resText_text)
-        headnode.appendChild(child_rs_resText) 
-
 def xml_except(result,headnode,xml_doc):
     rs_except = xml_doc.createElement('exception')
     rs_except_text1 = xml_doc.createTextNode('ERROR: %s'%str(result.exc_info[0]).replace('<','').replace('>',''))
@@ -144,11 +62,21 @@ def xml_except(result,headnode,xml_doc):
 
 class build:
     def __init__(self,result): 
-        try:
-           self.weave(result)
-	except:
-	   print 'error!!!!!!!!!!!'
+        self.weave(result)
 	
+
+    def getdumper(self,name):
+        from dumper import get_dumper,getRegdumpers
+        dumpers = getRegdumpers()
+	print dumpers
+	print dumpers.has_key(name)
+	print name
+        if dumpers.has_key(name):
+            dum = get_dumper(name)
+	else:
+	    dum = get_dumper('DEFAULT')
+	return dum()
+
     def weave(self,result):
         xml_doc = create_xml_doc()
         headnode = xml_head(result,xml_doc)
@@ -156,12 +84,16 @@ class build:
 	    if rs.nodetype in [u'sample','sample']:
 	         child = xml_sample(rs,headnode,xml_doc)
 		 if getattr(rs,'log',None):
-		     print '-------result----------'
-		     print rs.log
-                     if getattr(rs,'_sample',None) and rs._sample.type.lower() == 'soap':
-		         xml_soapsample_detail(rs,child,xml_doc)
-		     elif getattr(rs,'_sample',None) and rs._sample.type.lower() == 'http':
-		         xml_httpsample_detail(rs,child,xml_doc)
+		     #self.getdumpler(rs)
+		     try:
+                         demper = self.getdumper(rs._sample.type.upper())
+                         demper.dump(rs,headnode,xml_doc) 
+		     except:
+		         log.debug('have no dumper about  %s sampler'%rs._sample.type)
+                         print sys.exc_info()[0]
+                         print sys.exc_info()[1]
+                         for filename, lineno, function, msg in traceback.extract_tb(sys.exc_info()[2]):
+			     print '%s line %s in %s function [%s]'%(filename,lineno,function,msg)
 	         if rs.status in ['ERROR','FAIL']:
 		         xml_except(rs,child,xml_doc)
             else:
